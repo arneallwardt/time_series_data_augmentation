567138a78de0e030678275c37c6f4a7695abebbd
arneallwardt <132394779+arneallwardt@users.noreply.github.com>
2024-07-23 10:27:48 +0200

    - integrated new synthetic data and removed rain_1h outlier from traffic data - added brownian motion for traffic data

--- data_generation/algorithmic/brownian_motion.py
+++ data_generation/algorithmic/brownian_motion.py
@@ -1,39 +1,34 @@
 import pandas as pd
 import numpy as np
+from copy import deepcopy as dc
 
 rng = np.random.default_rng(42)
 
-def brownian_motion(df: pd.DataFrame, features=['Close']) -> pd.DataFrame:
+def brownian_motion(data: pd.DataFrame, features=['Close'], other_columns=[]) -> pd.DataFrame:
     '''
     Creates a new time series by applying brownian motion with mean and standard deviation from an existing time series
-    NOTE: this is probably not good for augmenting stock data since it is not stationary e.g. the standard deviation and variance change while time passes
+    Categorical columns are shuffled.
     '''
-    df = df.copy()
-    days = df.shape[0]
 
-    # remove unnecassary columns from original dataframe
-    features_incl_date = ['Date'] + features
-    df = df[features_incl_date]
+    dc_data = dc(data)
+    days = dc_data.shape[0]
 
     # check if all selected features are present in the dataframe
-    columns = df.columns.tolist()
-    for feature in features:
-        if feature not in columns:
-            raise ValueError(f'Dataframe does not include the following feature: {feature}.')
+    columns = dc_data.columns.tolist()
+    for col in features + other_columns:
+        if col not in columns:
+            raise ValueError(f'Dataframe does not include the following feature: {col}.')
         
+
     print(f'Augmenting the following columns with brownian motion: {features}...')
     for feature in features:
-        # calculate daily log returns and put them in an array
-        # df['LogReturns'] = np.log(df['Close'] / df['Close'].shift(1))
-        # log_returns = df['LogReturns'].dropna()
-
+        
         # calculate daily gains / losses and use them to calculate mean
-        df[f'Diff_{feature}'] = df[feature].diff()
-        diff = df[f'Diff_{feature}'].dropna()
+        dc_data[f'Diff_{feature}'] = dc_data[feature].diff()
+        diff = dc_data[f'Diff_{feature}'].dropna()
 
-        # calculate mean and standard deviation from log returns 
-        # mean, standard_deviation = log_returns.mean(), log_returns.std()
-        mean, standard_deviation = diff.mean(), diff.std()
+        # calculate mean and standard deviation
+        mean, standard_deviation = 0, diff.std()
 
         # get normal distributed values which represent the daily change for the specified feature of the augmented time series
         random_values = rng.normal(mean, standard_deviation, days)
@@ -43,26 +38,38 @@ def brownian_motion(df: pd.DataFrame, features=['Close']) -> pd.DataFrame:
 
         # create mock array for augmented prices and set first value to first closing price of the time series
         synthetic_values = np.zeros(days)
-        synthetic_values[0] = df.loc[0, feature]
+        synthetic_values[0] = dc_data.loc[0, feature]
         
         # create brownian motion
         for idx in range(days-1):
             # get real index, because we need real_idx - 1 later on 
             real_idx = idx + 1
+
             # calculate value for current index for every path of the matrix
-            synthetic_values[real_idx] = round(synthetic_values[real_idx - 1] + mean * step + standard_deviation * np.sqrt(step) * random_values[idx], 6)
+            # including drift term
+            # synthetic_values[real_idx] = synthetic_values[real_idx - 1] + mean * step + standard_deviation * np.sqrt(step) * random_values[idx], 6
+            # without drift term
+            synthetic_values[real_idx] = synthetic_values[real_idx - 1] + np.sqrt(step) * random_values[idx]
 
             # Volume coloumn allows only integer values
-            if feature == 'Volume':
-                synthetic_values[real_idx] = round(synthetic_values[real_idx])
+            if feature in ['traffic_volume', 'temp', 'rain_1h', 'snow_1h', 'clouds_all']:
+                # no negative values allowed
+                if synthetic_values[real_idx] < 0:
+                    synthetic_values[real_idx] = 0
 
-            # no negative values allowed
-            if synthetic_values[real_idx] < 0:
-                synthetic_values[real_idx] = 0
+            elif feature in ['clouds_all']:
+                if synthetic_values[real_idx] > 100:
+                    synthetic_values[real_idx] = 100
 
 
         # apply augmented prices and drop column which saves the diff
-        df[feature] = synthetic_values
-        df = df.drop(columns=[f'Diff_{feature}'])   
+        dc_data[feature] = synthetic_values
+        dc_data = dc_data.drop(columns=[f'Diff_{feature}'])   
 
-    return df
+    
+    # shuffle values of columns which cant be augmented via brownian motion since they are categorical
+    for col in other_columns:
+        dc_data[col] = dc_data[col].sample(frac=1).values
+        
+
+    return dc_data
