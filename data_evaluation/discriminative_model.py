35b2d7849beda6ee6ffb4f823cf259788f8ccc80
arneallwardt <132394779+arneallwardt@users.noreply.github.com>
2024-07-27 22:45:26 +0200

    organized results folder and added first discriminative results

--- data_evaluation/discriminative_model.py
+++ data_evaluation/discriminative_model.py
@@ -1,6 +1,15 @@
+import sys
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from tqdm import tqdm
+import numpy as np
+
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), '..'))
+sys.path.insert(0, parent_dir)
+
+from utilities import accuracy
 
 
 class LSTMClassification(nn.Module):
@@ -99,6 +108,9 @@ class CNNClassification(nn.Module):
         x = F.relu(self.conv3(x))
         print(f'After conv3: {x.shape}') if self.verbose else None
 
+        x = x.view(x.size(0), 5)
+        print(f'After view: {x.shape}') if self.verbose else None
+
         logits = self.linear(x)
         print(f'After linear: {logits.shape}') if self.verbose else None
 
@@ -107,3 +119,110 @@ class CNNClassification(nn.Module):
         
         pred_probs = torch.sigmoid(logits) # apply sigmoid activation function to get probabilities
         return pred_probs
+    
+
+def train_cnn(model, 
+              hyperparameters, 
+              train_loader, 
+              val_loader, 
+              criterion, 
+              optimizer,
+              patience=10):
+
+    '''
+    This function trains a CNN model on the training data and validates it on the validation data.
+    It also logs the training and validation loss and accuracy for each epoch.
+
+    Args:
+        model: torch.nn.Module object
+        hyperparameters: dict
+        train_loader: torch.utils.data.DataLoader object
+        val_loader: torch.utils.data.DataLoader object
+        criterion: torch.nn loss function
+        optimizer: torch.optim optimizer object
+        patience: int
+    '''
+
+    best_val_loss = np.inf
+    num_epochs_no_improvement = 0
+
+    train_losses = []
+    val_losses = []
+    train_accs = []
+    val_accs = []
+
+    for epoch in tqdm(range(hyperparameters['num_epochs'])):
+
+        ### Training ###
+        running_train_loss = 0
+        running_train_acc = 0
+
+        model.train()
+
+        for _, (X_batch_train, y_batch_train) in enumerate(train_loader):
+
+            X_batch_train = X_batch_train.float().to(hyperparameters['device'])
+
+            # forward pass
+            pred_logits_train = model(X_batch_train)
+
+            # get metrics
+            train_loss = criterion(pred_logits_train, y_batch_train)
+            train_acc = accuracy(y_true=y_batch_train, y_pred=torch.round(torch.sigmoid(pred_logits_train)))
+
+            # save metrics for running average and evaluation later on
+            running_train_loss += train_loss.item()
+            running_train_acc += train_acc
+            train_losses.append(train_loss.item())
+            train_accs.append(train_acc)
+
+            # gradient descent and backprop
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+
+
+        ### Validation ###
+        running_val_loss = 0
+        running_val_acc = 0
+
+        model.eval()
+        with torch.inference_mode():
+
+            for _, (X_batch_val, y_batch_val) in enumerate(val_loader):
+                X_batch_val = X_batch_val.float().to(hyperparameters['device'])
+
+                # forward pass
+                pred_logits_val = model(X_batch_val)
+
+                # get metrics
+                val_loss = criterion(pred_logits_val, y_batch_val)
+                val_acc = accuracy(y_true=y_batch_train, y_pred=torch.round(torch.sigmoid(pred_logits_train)))
+
+                # save metrics for running average and evaluation later on
+                running_val_loss += val_loss.item()
+                running_val_acc += val_acc
+                val_losses.append(val_loss.item())
+                val_accs.append(val_acc)
+
+
+            # Check for early stopping
+            if running_val_loss < best_val_loss:
+                best_val_loss = running_val_loss
+                num_epochs_no_improvement = 0
+
+            else:
+                print(f'INFO: Validation loss did not improve in epoch {epoch + 1}')
+                num_epochs_no_improvement += 1
+
+
+        ### Logging and Plotting ###
+
+        print(f'Epoch: {epoch} \n\b Train Loss: {running_train_loss / len(train_loader)} \n\b Train Acc: {running_train_acc / len(train_loader)} \n\b Val Loss: {running_val_loss / len(val_loader)} \n\b Val Acc: {running_val_acc / len(val_loader)}')
+        print('*' * 50)
+
+        if num_epochs_no_improvement >= patience:
+            print(f'Early stopping at epoch {epoch}')
+            break
+
+    return train_losses, val_losses, train_accs, val_accs, model
